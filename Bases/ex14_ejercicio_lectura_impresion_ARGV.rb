0fa6e0b2d39dbefee4fed10350c6e047e90ea41e
Crisel Ayala <criseljazminal@ufps.edu.co>
2020-04-16 18:36:56 -0500

    Update ex14_ejercicio_lectura_impresion_ARGV.rb

--- Bases/ex14_ejercicio_lectura_impresion_ARGV.rb
+++ Bases/ex14_ejercicio_lectura_impresion_ARGV.rb
@@ -26,7 +26,6 @@ You live in #{lives}. Not sure where that is.
 And you have a #{computer} computer. Nice.
 MESSAGE
 
-# Puedo comolar cualquier palabra en este tipo de impresiones
 puts <<CUALQUIER_COSA
 Alright, so you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
